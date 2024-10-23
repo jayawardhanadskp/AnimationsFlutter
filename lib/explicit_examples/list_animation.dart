a258e1f25905b2313b5b480ec65e16de07152a16
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-10-23 11:44:53 +0530

    slide animation (all the group of list come inside)

--- lib/explicit_examples/list_animation.dart
+++ lib/explicit_examples/list_animation.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
-class ListAnimation extends StatelessWidget {
+class ListAnimation extends StatefulWidget {
   const ListAnimation({super.key});
+
+  @override
+  State<ListAnimation> createState() => _ListAnimationState();
+}
+
+class _ListAnimationState extends State<ListAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<Offset> slideAnimation;
+
+  @override
+  void initState() {
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 5));
+
+    slideAnimation = Tween(begin: Offset(-1, 0), end: Offset(0, 0)).animate(controller);
+     
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +31,18 @@ class ListAnimation extends StatelessWidget {
       body: ListView.builder(
         itemCount: 5,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Hello World, Rivaan. ${index.toString()}'),
+          return SlideTransition(
+            position: slideAnimation,
+            child: ListTile(
+              title: Text('Hello World, Rivaan. ${index.toString()}'),
+            ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          controller.forward();
+        },
         child: const Icon(Icons.done),
       ),
     );
