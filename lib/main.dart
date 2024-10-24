ce7b2c1d73608c6354efae147bf3fd3dcb814a09
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-10-24 16:35:14 +0530

    animation only (not pageRouteBuilder)

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'explicit_examples/loading_animation.dart';
 import 'explicit_examples/login_animation.dart';
 import 'implicit_examples/animated_shopping_cart_button.dart';
 import 'implicit_examples/animated_tween_animation_builder_example.dart';
+import 'page_route_builder_animation/splash_animation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,10 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const RadialProgressAnimation(
-        progress: 0.65,
-        color: Colors.blue,
-      ),
+      home: const SplashAnimation()
     );
   }
 }
--- lib/page_route_builder_animation/splash_animation.dart
+++ lib/page_route_builder_animation/splash_animation.dart
@@ -1,23 +1,66 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class SplashAnimation extends StatelessWidget {
+class SplashAnimation extends StatefulWidget {
   const SplashAnimation({super.key});
+
+  @override
+  State<SplashAnimation> createState() => _SplashAnimationState();
+}
+
+class _SplashAnimationState extends State<SplashAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+
+    scaleAnimation = Tween<double>(begin: 1, end: 10).animate(controller);
+
+    controller.addListener(() {
+      if (controller.isCompleted) {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => const Destination()));
+        
+        // reset zoom after animation
+        Timer(const Duration(milliseconds: 500), () {
+          controller.reset();
+        });
+      }
+    });
+
+    
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: const BoxDecoration(
-            color: Colors.blue,
-            shape: BoxShape.circle,
-          ),
-          child: Container(
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blue,
+        child: GestureDetector(
+          onTap: () {
+            controller.forward();
+            // Navigator.push(context,MaterialPageRoute(builder: (context) => const Destination()));
+          },
+          child: ScaleTransition(
+            scale: scaleAnimation,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+              ),
+              child: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+              ),
             ),
           ),
         ),
